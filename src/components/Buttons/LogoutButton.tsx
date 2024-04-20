13db42dd93da1ee5175659da0dc28a4f35635af5
stevkim <stevenn.kkim@gmail.com>
2024-04-19 19:51:19 -0700

    Finished the flow for signup

--- src/components/Buttons/LogoutButton.tsx
+++ src/components/Buttons/LogoutButton.tsx
@@ -1,11 +1,16 @@
 'use client';
 import { signout } from '@/actions/authActions';
 import { toast } from 'react-hot-toast';
+import { useQueryClient } from '@tanstack/react-query';
 
 const LogoutButton = () => {
+	const queryClient = useQueryClient();
+
 	const logout = async () => {
 		await signout();
 		toast.success('Successfully Logged out');
+
+		queryClient.invalidateQueries({ queryKey: ['posts'] });
 	};
 
 	return <button onClick={logout}>logout</button>;
