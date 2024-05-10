bc1d0dbbe778333905e87cb7e758ab3d10784e67
Steven Kim <stevenn.kkim@gmail.com>
2024-05-10 14:50:05 -0700

    Added validation for content posts

--- src/lib/validateContent.ts
+++ src/lib/validateContent.ts
@@ -1,22 +1,51 @@
-import defaultValue, {
-	validateDefault,
-} from '../components/Editor/defaultValue';
+import defaultValue from '../components/Editor/defaultValue';
+
+const defaultStringList = new Set([
+	'Welcome to ',
+	'anon',
+	'!',
+	'Rules to Contribute',
+	'Respect: Be kind and refrain from offensive language',
+	'Original Content: Share your own writing; no plagiarism',
+	'Stay Appropriate: Follow legal and community standards',
+	'No Spamming: Keep posts relevant and avoid self-promotion',
+	"Honor Copyrights: Respect others' creative work",
+	'Embrace Anonymity: No names, no identities, ever',
+	'Enjoy Sharing: Inspire and be inspired by the community!',
+]);
+
+/*
+	checks to make sure the length of the new post is at least 3 lines
+	checks to make sure less than 20% of the lines match the default stringlist
+*/
 
 // returns true if valid, false if not valid
-export default function validateContent(content: typeof defaultValue) {
+export default function validateContent(content: typeof defaultValue): boolean {
 	// content must have at least 2 lines
 	if (content.content.length < 2) {
 		return false;
 	}
+	let similarStrings = 0;
 
 	const contentList: string[] = getList(content);
-	console.log(contentList);
+
+	for (let str of contentList) {
+		if (defaultStringList.has(str)) {
+			similarStrings++;
+		}
+	}
+
+	const similarity = similarStrings / defaultStringList.size;
+
+	// returns boolean: similarity to the default must be less than 20%
+	return similarity < 0.2;
 }
 
-function getList(content: typeof defaultValue) {
+// iterates the content and returns the string list
+function getList(content: typeof defaultValue): string[] {
 	const textList: string[] = [];
 
-	let stack: any[] = content.content;
+	let stack: any[] = [...content.content];
 
 	while (stack.length) {
 		const current = stack.shift();
@@ -33,5 +62,3 @@ function getList(content: typeof defaultValue) {
 
 	return textList;
 }
-
-console.log(validateContent(defaultValue));
