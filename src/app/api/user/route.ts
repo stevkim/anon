43c84cb9e8068b51fe849d4befa07fb9422d2ad8
Steven Kim <stevenn.kkim@gmail.com>
2024-06-02 16:14:19 -0700

    Bug fix - User posts that were added to saved posts were erroring out. Endpoint now queries for the user's saved posts as well

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,42 +1,53 @@
-import { NextResponse, type NextRequest } from 'next/server';
-import prisma from '@/db/client';
-import { createClient } from '@/utils/supabase/server';
-import { getUserPosts, getLikes } from '@/db/methods';
+import { NextResponse, type NextRequest } from "next/server";
+import prisma from "@/db/client";
+import { createClient } from "@/utils/supabase/server";
+import { getUserPosts, getLikes, getSaved } from "@/db/methods";
 
 // get Posts that the user has written
 // expects the pages from searchParams - /api/saved?page={PAGE}
 export async function GET(request: NextRequest) {
-	const supabase = createClient();
-	const LIMIT = 50;
-	const PAGE: number = Number(request.nextUrl.searchParams.get('page')) || 0;
+  const supabase = createClient();
+  const LIMIT = 50;
+  const PAGE: number = Number(request.nextUrl.searchParams.get("page")) || 0;
 
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-	try {
-		const userLikes = new Map();
+  try {
+    const userLikes = new Map();
+    const userSaved = new Map();
 
-		const [likes, posts] = await Promise.all([
-			getLikes(user!.id),
-			getUserPosts(user!.id, LIMIT, PAGE),
-		]);
+    const [likes, posts, saved] = await Promise.all([
+      getLikes(user!.id),
+      getUserPosts(user!.id, LIMIT, PAGE),
+      getSaved(user!.id),
+    ]);
 
-		likes.forEach((row) => {
-			userLikes.set(row.postId, row.id);
-		});
+    const maxLength = Math.max(likes.length, saved.length);
 
-		let results = posts.map((post) => ({
-			...post,
-			liked: userLikes.get(post.id),
-			authorId: 'author',
-		}));
+    // keeping track of the post that the user has liked/saved
+    for (let i = 0; i < maxLength; i++) {
+      if (likes[i]) {
+        userLikes.set(likes[i].postId, likes[i].id);
+      }
+      if (saved[i]) {
+        userSaved.set(saved[i].postId, saved[i].id);
+      }
+    }
 
-		return NextResponse.json({ data: results }, { status: 200 });
-	} catch (error) {
-		console.log(error);
-		return NextResponse.error();
-	} finally {
-		await prisma.$disconnect();
-	}
+    let results = posts.map((post) => ({
+      ...post,
+      liked: userLikes.get(post.id),
+      saved: userSaved.get(post.id),
+      authorId: "author",
+    }));
+
+    return NextResponse.json({ data: results }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error();
+  } finally {
+    await prisma.$disconnect();
+  }
 }
--- src/app/api/user/saved/[id]/route.ts
+++ src/app/api/user/saved/[id]/route.ts
@@ -1,56 +1,61 @@
-import { NextResponse, type NextRequest } from 'next/server';
-import { createClient } from '@/utils/supabase/server';
-import prisma from '@/db/client';
-import { revalidatePath } from 'next/cache';
+import { NextResponse, type NextRequest } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+import prisma from "@/db/client";
+import { revalidatePath } from "next/cache";
 
 // saves a post for a user
 // postId comes from the path - /api/saved/{postId}
 // userId is authenticated from the supabase auth middleware
 export async function POST(request: NextRequest) {
-	const supabase = createClient();
+  const supabase = createClient();
 
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-	const postId = request.nextUrl.pathname.split('/')[4];
+  const postId = request.nextUrl.pathname.split("/")[4];
 
-	try {
-		const record = await prisma.saved.create({
-			data: {
-				postId,
-				userId: user?.id as string,
-			},
-		});
+  console.log(postId);
 
-		revalidatePath('/saved');
+  try {
+    const record = await prisma.saved.create({
+      data: {
+        postId,
+        userId: user?.id as string,
+      },
+    });
 
-		return NextResponse.json({ data: record }, { status: 200 });
-	} catch (error) {
-		return NextResponse.error();
-	} finally {
-		await prisma.$disconnect();
-	}
+    console.log(record);
+
+    revalidatePath("/saved");
+
+    return NextResponse.json({ data: record }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error();
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 // removes a saved post from the user's list
 // expects a record id (id of the row for saved posts) in searchParams - /api/saved/{postId}?record={record}
 export async function DELETE(request: NextRequest) {
-	const record = request.nextUrl.searchParams.get('record') as string;
+  const record = request.nextUrl.searchParams.get("record") as string;
 
-	try {
-		await prisma.saved.delete({
-			where: {
-				id: record,
-			},
-		});
+  try {
+    await prisma.saved.delete({
+      where: {
+        id: record,
+      },
+    });
 
-		revalidatePath('/saved');
+    revalidatePath("/saved");
 
-		return NextResponse.json({}, { status: 200 });
-	} catch (error) {
-		return NextResponse.error();
-	} finally {
-		await prisma.$disconnect();
-	}
+    return NextResponse.json({}, { status: 200 });
+  } catch (error) {
+    return NextResponse.error();
+  } finally {
+    await prisma.$disconnect();
+  }
 }
