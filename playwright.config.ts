d61b686eaa2fc3c61a5e53c31440bb86c9b86d67
Steven Kim <stevenn.kkim@gmail.com>
2024-06-03 17:14:17 -0700

    Removed some comments for the playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig, devices } from "@playwright/test";
 import "dotenv/config";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./tests/e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -36,6 +28,7 @@ export default defineConfig({
       name: "chromium",
       use: {
         ...devices["Desktop Chrome"],
+        // Clipboard persmissions
         contextOptions: {
           permissions: ["clipboard-read", "clipboard-write"],
         },
@@ -53,26 +46,6 @@ export default defineConfig({
         ...devices["Desktop Safari"],
       },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
